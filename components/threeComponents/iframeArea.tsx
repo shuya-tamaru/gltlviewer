74d9236d1e1a9b9c241c6134574e6fe0d0520d4d
ShuyaTamaru <tamarushuuya@mba.local>
2022-11-18 17:30:14 +0900

    loading

--- components/threeComponents/iframeArea.tsx
+++ components/threeComponents/iframeArea.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
 import * as THREE from 'three';
 
 import BottomMenu from './BottomMenu';
 import Debug from './Debug';
 import DisplayCurrentFloor from './DisplayCurrentFloor';
 import Experience from './Experience';
+import LoadingDisplay from './LoadingDisplay';
 
 export default function IframeArea() {
   return (
@@ -27,8 +29,11 @@ export default function IframeArea() {
           }}
         >
           <Debug />
-          <Experience />
+          <Suspense fallback={null}>
+            <Experience />
+          </Suspense>
         </Canvas>
+        <LoadingDisplay />
         <BottomMenu />
         <DisplayCurrentFloor />
       </Box>
