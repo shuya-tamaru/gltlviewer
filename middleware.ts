a2c713399bb107ecd4b718c0b3b2fd8c53adee31
ShuyaTamaru <tamarushuuya@mba.local>
2022-12-24 12:26:10 +0900

    refact

--- middleware.ts
+++ middleware.ts
@@ -13,19 +13,9 @@ export function middleware(req: NextRequest) {
   if (basicAuth) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = Buffer.from(authValue, "base64").toString().split(":");
-    const buildingName = url.pathname.replace("/building/", "");
-    const matchList = buildingList.filter((obj) => {
-      if (obj.id === buildingName) return obj;
-    });
 
-    if (matchList.length > 0) {
-      if (user === matchList[0].user && pwd === matchList[0].password) {
-        return NextResponse.next();
-      }
-    } else {
-      if (user === `${process.env.NEXT_PUBLIC_LOCAL_ADMIN_UER}` && pwd === `${process.env.NEXT_PUBLIC_LOCAL_ADMIN_PASSWORD}`) {
-        return NextResponse.next();
-      }
+    if (user === `${process.env.NEXT_PUBLIC_LOCAL_ADMIN_UER}` && pwd === `${process.env.NEXT_PUBLIC_LOCAL_ADMIN_PASSWORD}`) {
+      return NextResponse.next();
     }
   }
   url.pathname = "/api/auth";
